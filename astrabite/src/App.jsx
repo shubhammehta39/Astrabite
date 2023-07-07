66bb7040cfd492da8a1b9c231553195a83c15ffa
shubham mehta <shubhammehta2003@gmail.com>
2023-07-07 16:53:42 +0530

    cham cham cham

--- astrabite/src/App.jsx
+++ astrabite/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 
 import About from './components/About'
+import Home from './components/Home';
+import Services from './components/Services';
 function App() {
 
   return (
@@ -10,6 +12,9 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<About/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/services" element={<Services/>} />
+
       </Routes>  
     </Router>
     </>
