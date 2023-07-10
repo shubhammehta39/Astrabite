3806eb59129419e05834ea513f3ebf6fd06b9ce7
Vatsal Shah <shahvatsal2003@gmail.com>
2023-07-10 17:43:02 +0530

    ..

--- astrabite/src/App.jsx
+++ astrabite/src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
-
+import Navbar from './components/Navbar';
 import About from './components/About'
 import Home from './components/Home';
 import Services from './components/Services';
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<About/>} />
         <Route path="/home" element={<Home/>} />
         <Route path="/services" element={<Services/>} />
-
+        <Route path='/navbar' element={<Navbar/>} />
       </Routes>  
     </Router>
     </>
