bdca09c4b289d8d88ad9f6386972332bed580a49
vatsalshah041 <shahvatsal2003@gmail.com>
2023-07-12 00:21:12 +0530

    aajka akhri

--- astrabite/src/components/OurTeam.jsx
+++ astrabite/src/components/OurTeam.jsx
@@ -1,18 +1,52 @@
 import React from 'react'
 import "./ourteam.css"
 import bruh from "../assets/bruh.jpg"
-import { Grid, IconButton, Typography, Button } from '@mui/material'
-;const OurTeam = () => {
+import { useEffect,useState } from 'react';
+import { Grid } from '@mui/material';
+import axios from 'axios';
+const OurTeam = () => {
+  const [data,setData]=useState();
+  useEffect(() => {
+
+
+let config = {
+  method: 'get',
+  maxBodyLength: Infinity,
+  url: 'https://astrabyte.pythonanywhere.com/slave/',
+  headers: { }
+};
+
+async function makeRequest() {
+  try {
+    const response = await axios.request(config);
+    console.log((response.data));
+    setData(response.data);
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+makeRequest();
+
     
+  }, [])
+      
   return (
     <>
-      <Grid container spacing={3}></Grid>
-    <div class="p-10">
-      <h3 class='pb-20 font-bold text-5xl'>Meet our Team</h3>
+    <h3 class='pb-20 font-bold text-5xl'>Meet our Team</h3>
+      <Grid container spacing={3} sx={{justifyContent:"Center",display:"flex",alignItems:"center"}}>
+        {data?<>
+        {data.map((item)=>{
+          return(
+            <>
+            <Grid item md={4} xs={12} sm={12}>
+        <div class="p-10">
+      
       <div class="card">
-  <b></b>
+  <b style={{backgroundImage: `url(${item.image})` }}></b>
   <div class="content" >
-    <p class="title">Gaj Joshi<br /><span>Founder</span></p>
+    <p class="title">{item.name}<br /><span>{item.exp}</span></p>
     <ul class="sci">
       <li>
         <a href="#">
@@ -27,13 +61,13 @@ import { Grid, IconButton, Typography, Button } from '@mui/material'
       </li>
 
       <li>
-        <a href="#">
+        <a href={item.linkedin_url} target='_blank'>
           <svg class="fa-brands fa-linkedin-in" width="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path></svg>
         </a>
       </li>
 
       <li>
-        <a href="#">
+        <a href={item.insta_id} target='_blank'>
           <svg class="fa-brands fa-instagram" width="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path></svg>
         </a>
       </li>
@@ -42,6 +76,17 @@ import { Grid, IconButton, Typography, Button } from '@mui/material'
 </div>
 
     </div>
+        </Grid>
+
+
+            </>
+          )
+        })}
+        
+        </>:<></>}
+        
+      </Grid>
+    
 
     </>
   )
