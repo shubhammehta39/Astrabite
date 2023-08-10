e2df99d3c25b11c2d3f5727c4990ab0f713a008b
Vatsal Shah <shahvatsal2003@gmail.com>
2023-08-10 14:01:07 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
     <>
-
+      <Router>
+        <Footer/>
+      </Router>  
     </>
   )
 }
