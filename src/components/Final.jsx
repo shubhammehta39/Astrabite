3f5ff2d7b01fbfd49b23a629858055de443e63f6
shubham mehta <shubhammehta2003@gmail.com>
2023-08-16 13:57:49 +0530

    service add

--- src/components/Final.jsx
+++ src/components/Final.jsx
@@ -4,21 +4,80 @@ import "./final.css";
 const Final = () => {
   return (
     <div className="hi">
-      <div className="container">
-        <div className="background-image"></div>
-        <div className="content">
-          <div className="image-div">
-            <img src="../src/assets/char.png" alt="Content" />
-          </div>
-          <div className="image-div ">
+      <div style={{ display: "flex", flexDirection: "column", gap: "150px" }}>
+        <div>
+          <div className="container">
+            <div className="background-image"></div>
+            <div className="content">
+              <div className="image-div">
+                <img src="../src/assets/char.png" alt="Content" />
+              </div>
+              {/* <div className="image-div ">
             <img src="../src/assets/banner.png" alt="Content" />
+          </div> */}
+              <div className="text-div">
+                <h1 class="text-7xl font-bold font-stretch-10px text-center align-center justify-center">
+                  Gaming 
+                </h1>
+                <p>
+                  Your text description goes here.
+                  <br />
+                  Your text description goes here. <br />
+                  Your text description goes here.
+                </p>
+              </div>
+            </div>
           </div>
-          {/* <div className="text-div">
-           <p>Your text description goes here.
-            <br />Your text description goes here. <br />Your text description goes here.
-          </p> 
-          <h1 class="text-9xl font-bold font-stretch-10px text-center align-center justify-center">Astrabite</h1>
-        </div> */}
+        </div>
+        <div>
+          <div className="container">
+            <div className="background-image"></div>
+            <div className="content">
+            <div className="text-div">
+                <h1 class="text-7xl font-bold font-stretch-10px text-center align-center justify-center">
+                  3D Printing
+                </h1>
+                <p>
+                  Your text description goes here.
+                  <br />
+                  Your text description goes here. <br />
+                  Your text description goes here.
+                </p>
+              </div>
+              <div className="image-div">
+                <img src="../src/assets/char.png" alt="Content" />
+              </div>
+              {/* <div className="image-div ">
+            <img src="../src/assets/banner.png" alt="Content" />
+          </div> */}
+             
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="container">
+            <div className="background-image"></div>
+            <div className="content">
+              <div className="image-div" class="w-'25%'">
+                <img src="../src/assets/char.png" alt="Content" />
+              </div>
+              {/* <div className="image-div ">
+            <img src="../src/assets/banner.png" alt="Content" />
+          </div> */}
+              <div className="text-div">
+                <h1 class="text-7xl font-bold font-stretch-10px text-center align-center justify-center">
+                  Studios
+                </h1>
+                <p>
+                  Your text description goes here.
+                  <br />
+                  Your text description goes here. <br />
+                  Your text description goes here.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
