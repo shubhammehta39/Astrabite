317861f985fdfaa3b6d8667935a472ebd4974b74
shubham mehta <shubhammehta2003@gmail.com>
2023-08-17 18:15:25 +0530

    ...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import logo from '../assets/logo.png'
 import logon from '../assets/logon.png'
 export default function Navbar() {
+  const styles={
+    position: "fixed",
+    top: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "white",
+    background: "rgba(231, 227, 227, 0.35)",
+    boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
+    backdropFilter: "blur(6.5px)",
+    webkitBackdropFilter: "blur(11px)",
+    border: "1px solid rgba(255, 255, 255, 0.18)",
+    zIndex: 999,
+  }
   return (
+    
     <>
-    <div style={{position:"sticky",zIndex:"5  ",top:0,backgroundColor:"",paddingTop:"0px"}}>
+    <div style={styles}>
       <header class="w-full h-16 flex justify-center  " >
         <nav class="w-4/5 flex justify-between items-center font-bebas">
           <h1 class="text-3xl text-color-black"><img src={logon} alt="" style={{width:"50px"}}/></h1>
--- src/components/Newservice.jsx
+++ src/components/Newservice.jsx
@@ -3,115 +3,160 @@ import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import { Button } from "@mui/material";
+import Typography from "@mui/material/Typography";
 import "./final.css";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+// import Navb
 
 const Newservice = () => {
   return (
-    <div>
-      <Grid
-        container
-        style={{ padding: "10%", margin: "0" }}
-      >
-        <Grid item md={6} spacing={3} style={{ padding: "10%" }}>
-          <Grid container spacing={3}>
-            <Grid item md={12}>
-              Service 1
-            </Grid>
-            <Grid item md={12}>
-              uska thoda desc
-            </Grid>
-            <Grid item md={12}>
-              {" "}
-              <Button
-                variant="filed"
-                sx={{ color: "white ", bgcolor: "#FC6709" }}
-              >
-                {" "}
-                click me{" "}
-              </Button>
-            </Grid>
-          </Grid>
-        </Grid>
+    <>
+      <Navbar />
+      <div style={{}}>
 
         
-        <Grid item md={6}>
-          <div className="bb">
-            <div className="content">
-              <img
-                src="../src/assets/3dprinter.png"
-                alt="hi"
-                style={{ width: "80%" }}
-              />
-            </div>
-          </div>
-        </Grid>
-      </Grid>
 
-      <Grid
-        container
-        style={{ padding: "10%", border: "1px solid white", margin: "0" }}
-      >
-        <Grid item md={6}>
-          <div className="bb1">
-            <div className="content">
-              <img
-                src="../src/assets/Char.png"
-                alt="hi"
-                style={{ width: "80%" }}
-              />
-            </div>
-          </div>
-        </Grid>
-        <Grid item md={6} spacing={3} style={{ padding: "10%" }}>
-          <Grid container spacing={3}>
-            <Grid item md={12}>
-              Service 1
-            </Grid>
-            <Grid item md={12}>
-              uska thoda desc
-            </Grid>
-            <Grid item md={12}>
-              {" "}
-              <Button
-                variant="filed"
-                sx={{ color: "white ", bgcolor: "#FC6709" }}
-              >
+        <Grid container style={{ padding: "5%", height: "150%", margin: "0" }}>
+          <Grid
+            item
+            md={6}
+            spacing={3}
+            style={{
+              padding: "10%",
+              borderWidth: "1px",
+              borderStyle: "outset",
+              borderRight: "none",
+            }}
+          >
+            <Grid container spacing={3}>
+              <Grid item md={12}>
+                <Typography variant="h2">Gaming</Typography>
+              </Grid>
+              <Grid item md={12}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo
+                est natus saepe. Minus natus veritatis beatae suscipit saepe
+                error perferendis Lorem ipsum dolor sit, amet consectetur
+                adipisicing elit. Ut, praesentium.
+              </Grid>
+              <Grid item md={12}>
                 {" "}
-                click me{" "}
-              </Button>
+                <Button
+                  variant="filed"
+                  sx={{ color: "white ", bgcolor: "#FC6709" }}
+                >
+                  {" "}
+                  click me{" "}
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+
+          <Grid
+            item
+            md={6}
+            sx={{
+              borderStyle: "outset",
+              borderWidth: "1px",
+              borderLeft: "none",
+            }}
+          >
+            <div className="bb">
+              <div className="content">
+                <img
+                  src="../src/assets/3dprinter.png"
+                  alt="hi"
+                  style={{ width: "80%" }}
+                />
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+
+        <Grid
+          container
+          style={{ padding: "5%",  margin: "0" }}
+        >
+          <Grid item md={6} style={{
+              padding: "10%",
+              borderWidth: "1px",
+              borderStyle: "outset",
+              borderRight: "none",
+            }}>
+            <div className="bb1">
+              <div className="content">
+                <img
+                  src="../src/assets/Char.png"
+                  alt="hi"
+                  style={{ width: "80%" }}
+                />
+              </div>
+            </div>
+          </Grid>
+          <Grid item md={6} spacing={3} sx={{
+              borderStyle: "outset",
+              borderWidth: "1px",
+              borderLeft: "none",
+            }}>
+            <Grid container spacing={3} sx={{paddingTop:"30%"}}>
+              <Grid item md={12} >
+                <Typography variant="h2">3d Printing</Typography>
+              </Grid>
+              <Grid item md={12}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                suscipit eligendi officiis iste fugit dolorem obcaecati corporis
+                aspernatur, veritatis accusamus quisquam, tenetur laboriosam
+                ipsam cumque? Ducimus dicta delectus laboriosam minus!
+              </Grid>
+              <Grid item md={12}>
+                {" "}
+                <Button
+                  variant="filed"
+                  sx={{ color: "white ", bgcolor: "#FC6709" }}
+                >
+                  {" "}
+                  click me{" "}
+                </Button>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
 
-        
-      </Grid>
-
-      <Grid
-        container
-        style={{ padding: "10%", border: "1px solid white", margin: "0" }}
-      >
-        <Grid item md={6} spacing={3} style={{ padding: "10%" }}>
-          <Grid container spacing={3}>
-            <Grid item md={12}>
-              Service 1
-            </Grid>
-            <Grid item md={12}>
-              uska thoda desc
-            </Grid>
-            <Grid item md={12}>
-              {" "}
-              <Button
-                variant="filed"
-                sx={{ color: "white ", bgcolor: "#FC6709" }}
-              >
+        <Grid
+          container
+          style={{ padding: "5%", margin: "0" }}
+        >
+          <Grid item md={6} spacing={3} style={{
+              padding: "10%",
+              borderWidth: "1px",
+              borderStyle: "outset",
+              borderRight: "none",
+            }}>
+            <Grid container spacing={3}>
+              <Grid item md={12}>
+                <Typography variant="h2">Studios</Typography>
+              </Grid>
+              <Grid item md={12}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Eligendi hic quidem totam assumenda itaque enim mollitia,
+                quaerat provident tempora aperiam cumque consequatur, asperiores
+                dolor tenetur. Necessitatibus natus facilis temporibus
+                reiciendis!
+              </Grid>
+              <Grid item md={12}>
                 {" "}
-                click me{" "}
-              </Button>
+                <Button
+                  variant="filed"
+                  sx={{ color: "white ", bgcolor: "#FC6709" }}
+                >
+                  {" "}
+                  click me{" "}
+                </Button>
+              </Grid>
             </Grid>
           </Grid>
-        </Grid>
 
-        {/* <Grid item md={6} style={{ border: "1px solid white" }}>
+          {/* <Grid item md={6} style={{ border: "1px solid white" }}>
             <div className="bb">
     
             <div>
@@ -124,19 +169,25 @@ const Newservice = () => {
             </div></div>
         </Grid>
       </Grid> */}
-        <Grid item md={6}>
-          <div className="bb">
-            <div className="content">
-              <img
-                src="../src/assets/3dprinter.png"
-                alt="hi"
-                style={{ width: "80%" }}
-              />
+          <Grid item md={6} sx={{
+              borderStyle: "outset",
+              borderWidth: "1px",
+              borderLeft: "none",
+            }}>
+            <div className="bb">
+              <div className="content">
+                <img
+                  src="../src/assets/3dprinter.png"
+                  alt="hi"
+                  style={{ width: "80%" }}
+                />
+              </div>
             </div>
-          </div>
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
+      </div>
+      {/* <Footer/> */}
+    </>
   );
 };
 
