cc1513fc5217dff6b733c7d3b4ec40b3c5fbed56
shubham mehta <shubhammehta2003@gmail.com>
2023-08-24 17:02:31 +0530

    waving astronaut

--- astrabite/src/components/MaintainancePg.jsx
+++ astrabite/src/components/MaintainancePg.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Box, Typography, CircularProgress } from "@mui/material";
 import './shi.css';
+import astraunot from '../assets/Wave white.mp4';
+
 
 const MaintainancePg = () => {
   return (
@@ -12,7 +14,8 @@ const MaintainancePg = () => {
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
-        backgroundColor: "#f5f5f5",
+        backgroundColor: "#fff",
+        
       }}
     >
       {/* <Box
@@ -27,21 +30,28 @@ const MaintainancePg = () => {
         }
       } /> */}
 
-        <div className="animation">
+        <div className="animation" style={{zIndex:'999'}}>
         <div className="one spin-one"></div>
         <div className="two spin-two"></div>
         <div className="three spin-one"></div>
        </div>
       
         {/* <CircularProgress size={64} color="error" /> */}
-        <Typography variant="h5" component="h1" align="center" mt={2} style={{color:'black' , paddingTop:'10px', paddingBottom:'10px'}}>
+        <Typography variant="h5" component="h1" align="center" mt={2} style={{color:'black' , paddingTop:'10px', paddingBottom:'10px',zIndex:'999'}}>
           Maintenance in Progress
         </Typography>
-        <Typography variant="body1" align="center" mt={2} style={{color:'black' , paddingBottom:'10px'}}>
+        <Typography variant="body1" align="center" mt={2} style={{color:'black' , paddingBottom:'10px', width:'380px',zIndex:'999'}}>
           We apologize for the inconvenience. The website is currently
           undergoing maintenance. Please check back later.
         </Typography>
+       <div>
+       <video autoPlay loop muted className="video" mt={2} style={{position:"absolute",left:0,bottom:0}}>
+                  <source src={astraunot} type="video/mp4" />
+                </video>
+       </div>
       </Box>
+
+
     {/* </Box> */}
     </div>
   )
